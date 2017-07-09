67d6afd1bb5a76eb49d8873f0ba202b979bf8857
Lucient Lau <pursueexcel@gmail.com>
2017-07-09 17:58:57 +0800

    Modify classifier to enhighce learn performace

--- DimenBroadcast/ClassifierTest.py
+++ DimenBroadcast/ClassifierTest.py
@@ -22,27 +22,22 @@ print(n_classes,n_features)
 X_train = tf.placeholder("float32",[None,n_features])
 y_train = tf.placeholder("float32",[None])
 
-weights_value = tf.Variable(tf.zeros([n_features,n_classes]))
+weights_value = tf.Variable(tf.random_normal([n_features,n_classes]))
 
-biase_value = tf.Variable(tf.zeros([n_classes]))
-
-
-y_pre = tf.nn.softmax(tf.matmul(X_train,weights_value) + biase_value)
-
-loss_function = tf.reduce_mean(tf.abs((y_train-y_pre)/y_train))
-
-train_op = tf.train.GradientDescentOptimizer(0.1).minimize(loss_function)
+biase_value = tf.Variable(tf.random_normal([n_classes]))
 
 
+y_pre = (tf.matmul(X_train,weights_value) + biase_value)
 
+loss_function = tf.reduce_mean(tf.abs((y_train-y_pre)))
 
+train_op = tf.train.GradientDescentOptimizer(0.001).minimize(loss_function)
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	for i in range(10000):
-		sess.run(train_op,feed_dict = {X_train:X,y_train:y})
-		if i%100 == 0 :
-			print("Iteration{}:\tLoss={:.6f}".format(i,sess.run(loss_function,{X_train:X,y_train:y})))
+	for i in range(1000):
+		sess.run(train_op,feed_dict = {X_train:X,y_train:y})	
+		print("Iteration{}:\tLoss={:.6f}".format(i,sess.run(loss_function,{X_train:X,y_train:y})))
 
 
 
