d6b02f74d27b708ef7bb4de1100aecd93f7725cd
Lucient Lau <pursueexcel@gmail.com>
2017-08-26 21:17:13 +0800

    modify summary code but it not necessary [the resone that i cant see the summary data is because that i dont use full path for tensorboard logdir]

--- DimenBroadcast/MNIST/mnist.py
+++ DimenBroadcast/MNIST/mnist.py
@@ -136,21 +136,22 @@ def train():
 
     with tf.name_scope('summaries'):
 
-      mean = tf.reduce_mean(var)
 
-      tf.summary.scalar('mean', mean)
+      	mean = tf.reduce_mean(var)
 
-      with tf.name_scope('stddev'):
+      	tf.summary.scalar('mean', mean)
 
-        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+      	with tf.name_scope('stddev'):
 
-      tf.summary.scalar('stddev', stddev)
+        	stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
 
-      tf.summary.scalar('max', tf.reduce_max(var))
+      	tf.summary.scalar('stddev', stddev)
 
-      tf.summary.scalar('min', tf.reduce_min(var))
+      	tf.summary.scalar('max', tf.reduce_max(var))
 
-      tf.summary.histogram('histogram', var)
+      	tf.summary.scalar('min', tf.reduce_min(var))
+
+      	tf.summary.histogram('histogram', var)
 
 
 
@@ -181,6 +182,7 @@ def train():
         variable_summaries(weights)
 
       with tf.name_scope('biases'):
+      	
 
         biases = bias_variable([output_dim])
 
@@ -286,7 +288,7 @@ def train():
 
   tf.global_variables_initializer().run()
 
-
+  embedding_var = tf.get_variable(....)
 
   # Train the model, and also write summaries.
 
