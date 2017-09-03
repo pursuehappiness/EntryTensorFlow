524036939fe582e0aa9b84a5fca427f2464e370e
Lucient Lau <pursueexcel@gmail.com>
2017-09-03 13:42:50 +0800

    list the build steps of tensorflow model structure

--- tensorflow_ml/denselearn.py
+++ tensorflow_ml/denselearn.py
@@ -6,5 +6,26 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_boolean("boolean_value",False,"test flags")
 
+
+def tflearn():
+	#first step parser input arguments
+
+
+	#then loader the saved model
+
+
+	#after that build a model
+
+
+	#loade train data
+
+	#loade test data
+
+	#train the model
+
+	#validate the trained model accuracy
+
+	#export the model
+
 if __name__ == "__main__":
 	print(flags.FLAGS.boolean_value)
